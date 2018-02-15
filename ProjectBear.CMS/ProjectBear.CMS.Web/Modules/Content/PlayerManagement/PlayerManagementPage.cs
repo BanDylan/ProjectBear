8435ab7dfe838cbb47e5fa8b9616fb4f1d797bb1
Andrew Howard <andrew.howard@fullstack.co.za>
2018-02-15 08:48:19 +0200

    Implemented Player strikes and banning.

--- ProjectBear.CMS/ProjectBear.CMS.Web/Modules/Content/PlayerManagement/PlayerManagementPage.cs
+++ ProjectBear.CMS/ProjectBear.CMS.Web/Modules/Content/PlayerManagement/PlayerManagementPage.cs
@@ -44,8 +44,61 @@ namespace ProjectBear.CMS.Modules.Content.PlayerManagement
         [HttpGet, PageAuthorize("Administration")]
         public ActionResult ViewPlayer(Guid id)
         {
-            var player = new PlayerViewModel(id, "", true);
+            var player = new PlayerViewModel(id, "", true, true);
             return View("~/Modules/Content/PlayerManagement/PlayerManagementForm.cshtml", player);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [PageAuthorize("Administration")]
+        public bool ToggleBan(Guid profileId)
+        {
+            var profile = db.Profile.FirstOrDefault(x => x.ProfileId == profileId);
+            profile.Banned = !profile.Banned;
+            if(profile.Banned)
+            {
+                var now = DateTime.Now;
+                var bookings = profile.PlayerSlots.Where(x => x.TimeSlot.Roster.Date.AddMinutes(x.TimeSlot.Offset) >= now).ToList();
+                for(int i = 0; i < bookings.Count; i++)
+                {
+                    db.Entry(bookings[i]).State = EntityState.Deleted;
+                }
+ 
+                var reserves = profile.ReserveSlots.Where(x => x.TimeSlot.Roster.Date.AddMinutes(x.TimeSlot.Offset) >= now).ToList();
+                for (int i = 0; i < reserves.Count; i++)
+                {
+                    db.Entry(reserves[i]).State = EntityState.Deleted;
+                }
+            }
+            db.Entry(profile).State = EntityState.Modified;
+            db.SaveChanges();
+            return profile.Banned;
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [PageAuthorize("Administration")]
+        public int RemoveStrike(Guid profileId)
+        {
+            var profile = db.Profile.FirstOrDefault(x => x.ProfileId == profileId);
+            if (profile.Strikes > 0)
+                profile.Strikes--;
+            db.Entry(profile).State = EntityState.Modified;
+            db.SaveChanges();
+            return profile.Strikes;
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [PageAuthorize("Administration")]
+        public int AddStrike(Guid profileId)
+        {
+            var profile = db.Profile.FirstOrDefault(x => x.ProfileId == profileId);
+            profile.Strikes++;
+            db.Entry(profile).State = EntityState.Modified;
+            db.SaveChanges();
+            return profile.Strikes;
         }
 
     }
--- ProjectBear.CMS/ProjectBear.CMS.Web/ViewModels/PlayerViewModel.cs
+++ ProjectBear.CMS/ProjectBear.CMS.Web/ViewModels/PlayerViewModel.cs
@@ -15,80 +15,82 @@ namespace ProjectBear.CMS.ViewModels
         public List<PlayerNameViewModel> PlayerNames { get; set; }
         public List<PlayerBookingViewModel> GameBookings { get; set; }
 
-        public string LastPlayed => GameBookings?.FirstOrDefault()?.Date.ToString("dd/MM/yyyy HH:mm") ?? "Never booked";
+        //public string LastPlayed => GameBookings?.FirstOrDefault()?.Date.ToString("dd/MM/yyyy HH:mm") ?? "Never booked";
 
-        public PlayerViewModel(Guid profileId, string matchingPlayerName, bool showCurrentSteamName)
+        public PlayerViewModel(Guid profileId, string matchingPlayerName, bool showCurrentSteamName, bool getFullData = false)
         {
             CurrentSteamName = db.ProfileSteamName.Where(x => x.ProfileId == profileId).OrderByDescending(y => y.FirstUsedDate).FirstOrDefault().SteamName;
             MatchingPlayerName = showCurrentSteamName ? CurrentSteamName : matchingPlayerName;
             PlayerProfile = db.Profile.FirstOrDefault(x => x.ProfileId == profileId);
+
             PlayerNames = new List<PlayerNameViewModel>();
-
-            var steamNames = db.ProfileSteamName.Where(x => x.ProfileId == profileId);
-
-            foreach(var steamName in steamNames)
-            {
-                if (!PlayerNames.Any(x => x.Name == steamName.SteamName))
-                {
-                    PlayerNames.Add(new PlayerNameViewModel
-                    {
-                        Name = steamName.SteamName,
-                    });
-                }
-                    
-            }
-
-            var nonSteamGameBookings = db.PlayersInTimeSlot.Where(x => x.ProfileId == profileId && !x.TimeSlot.IsSteamGame).ToList();
-            foreach(var nonSteamGameBooking in nonSteamGameBookings)
-            {
-                if(!PlayerNames.Any(x => x.Name == nonSteamGameBooking.NonSteamName))
-                {
-                    PlayerNames.Add(new PlayerNameViewModel
-                    {
-                        Name = nonSteamGameBooking.NonSteamName,
-                    });
-                }                
-            }
-
-            PlayerNames = PlayerNames.OrderBy(x => x.Name).ToList();
-
             GameBookings = new List<PlayerBookingViewModel>();
-            var bookings = db.PlayersInTimeSlot.Where(x => x.ProfileId == profileId).ToList();
 
-            foreach(var booking in bookings)
+            if (getFullData)
             {
-                var bookingStart = booking.TimeSlot.Roster.Date.AddMinutes(booking.TimeSlot.Offset);
-                if (booking.TimeSlot.IsSteamGame)
+                var steamNames = db.ProfileSteamName.Where(x => x.ProfileId == profileId);
+
+                foreach (var steamName in steamNames)
                 {
-                    var name = "";
-                    foreach(var steamName in steamNames.OrderBy(x => x.FirstUsedDate)) {
-                        if (steamName.FirstUsedDate <= bookingStart)
-                            name = steamName.SteamName;
-                        else
-                            break;
+                    if (!PlayerNames.Any(x => x.Name == steamName.SteamName))
+                    {
+                        PlayerNames.Add(new PlayerNameViewModel
+                        {
+                            Name = steamName.SteamName,
+                        });
                     }
 
-                    GameBookings.Add(new PlayerBookingViewModel
-                    {
-                        Date = bookingStart,
-                        Game = booking.TimeSlot.GameName,
-                        PlayerName = name,
-                        DidNotPitch = booking.DidNotPitch,
-                    });
                 }
-                else
+                var nonSteamGameBookings = db.PlayersInTimeSlot.Where(x => x.ProfileId == profileId && !x.TimeSlot.IsSteamGame).ToList();
+                foreach (var nonSteamGameBooking in nonSteamGameBookings)
                 {
-                    GameBookings.Add(new PlayerBookingViewModel
+                    if (!PlayerNames.Any(x => x.Name == nonSteamGameBooking.NonSteamName))
                     {
-                        Date = bookingStart,
-                        Game = booking.TimeSlot.GameName,
-                        PlayerName = booking.NonSteamName,
-                        DidNotPitch = booking.DidNotPitch,
-                    });
+                        PlayerNames.Add(new PlayerNameViewModel
+                        {
+                            Name = nonSteamGameBooking.NonSteamName,
+                        });
+                    }
                 }
-            }
+                PlayerNames = PlayerNames.OrderBy(x => x.Name).ToList();
 
-            GameBookings = GameBookings.OrderByDescending(x => x.Date).ToList();           
+                var bookings = PlayerProfile.PlayerSlots.ToList();//db.PlayersInTimeSlot.Where(x => x.ProfileId == profileId).ToList();
+                foreach (var booking in bookings)
+                {
+                    var bookingStart = booking.TimeSlot.Roster.Date.AddMinutes(booking.TimeSlot.Offset);
+                    if (booking.TimeSlot.IsSteamGame)
+                    {
+                        var name = "";
+                        foreach (var steamName in steamNames.OrderBy(x => x.FirstUsedDate))
+                        {
+                            if (steamName.FirstUsedDate <= bookingStart)
+                                name = steamName.SteamName;
+                            else
+                                break;
+                        }
+
+                        GameBookings.Add(new PlayerBookingViewModel
+                        {
+                            Date = bookingStart,
+                            Game = booking.TimeSlot.GameName,
+                            PlayerName = name,
+                            DidNotPitch = booking.DidNotPitch,
+                        });
+                    }
+                    else
+                    {
+                        GameBookings.Add(new PlayerBookingViewModel
+                        {
+                            Date = bookingStart,
+                            Game = booking.TimeSlot.GameName,
+                            PlayerName = booking.NonSteamName,
+                            DidNotPitch = booking.DidNotPitch,
+                        });
+                    }
+                }
+                GameBookings = GameBookings.OrderByDescending(x => x.Date).ToList();
+            }
+            
         }      
     }
 }
